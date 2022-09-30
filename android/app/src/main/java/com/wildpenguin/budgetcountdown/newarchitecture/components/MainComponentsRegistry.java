3025e9e3e68c5bab00f85d09825e82806a929831
penguin <fake@email.com>
2022-09-30 13:07:16 +0200

    feat: add licence file

--- android/app/src/main/java/com/wildpenguin/budgetcountdown/newarchitecture/components/MainComponentsRegistry.java
+++ android/app/src/main/java/com/wildpenguin/budgetcountdown/newarchitecture/components/MainComponentsRegistry.java
@@ -1,4 +1,4 @@
-package com.budgetcountdown.newarchitecture.components;
+package com.wildpenguin.budgetcountdown.newarchitecture.components;
 
 import com.facebook.jni.HybridData;
 import com.facebook.proguard.annotations.DoNotStrip;
--- android/app/src/main/java/com/wildpenguin/budgetcountdown/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
+++ android/app/src/main/java/com/wildpenguin/budgetcountdown/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
@@ -1,4 +1,4 @@
-package com.budgetcountdown.newarchitecture.modules;
+package com.wildpenguin.budgetcountdown.newarchitecture.modules;
 
 import com.facebook.jni.HybridData;
 import com.facebook.react.ReactPackage;
